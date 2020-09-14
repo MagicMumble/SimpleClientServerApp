29e47aca84b64a8d398a5b65dae600b57558ad8f
MagicMumble <anyabalerina99@mail.ru>
2020-09-14 13:48:48 +0300

    release v1

---
+++ proto/proto.go
@@ -0,0 +1,41 @@
+package proto
+
+import "encoding/json"
+
+// Request -- запрос клиента к серверу.
+type Request struct {
+	// Поле Command может принимать три значения:
+	// * "quit" - прощание с сервером (после этого сервер рвёт соединение);
+	// * "set new array" - передача нового массива на сервер;
+	// * "sort" - отсортировать массив
+	Command string `json:"command"`
+
+	// Если Command == "set new array", в поле Data должен лежать массив
+	// В противном случае, поле Data пустое.
+	Data *json.RawMessage `json:"data"`
+}
+
+// Response -- ответ сервера клиенту.
+type Response struct {
+	// Поле Status может принимать три значения:
+	// * "ok" - успешное выполнение команды "quit" или "sort";
+	// * "failed" - в процессе выполнения команды произошла ошибка;
+	// * "result" - массив отсортирован.
+	Status string `json:"status"`
+
+	// Если Status == "failed", то в поле Data находится сообщение об ошибке.
+	// Если Status == "result", в поле Data должна лежать дробь
+	// в виде структуры Fraction.
+	// В противном случае, поле Data пустое.
+	Data *json.RawMessage `json:"data"`
+}
+
+// Fraction -- дробь.
+//type Fraction struct {
+//	// Числитель (в десятичной системе, разрешён знак).
+//	Numerator string `json:"num"`
+//
+//	// Знаменатель (в десятичной системе).
+//	Denominator string `json:"denom"`
+//}
+
